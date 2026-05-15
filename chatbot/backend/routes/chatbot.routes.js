faa7c0398e115304cc8196ced911f852f68c8dc1
Shlok Mishra <shlokmishra576@gmail.com>
2026-05-15 19:47:02 +0530

    Clean up old files and prepare Shoes_web upload

--- chatbot/backend/routes/chatbot.routes.js
+++
@@ -1,7 +0,0 @@
-import  express from "express"
-import { Message } from "../Controllers/chatbot.message.js"
-
-const router = express.Router()
-router.post("/message" , Message)
-
-export default router
